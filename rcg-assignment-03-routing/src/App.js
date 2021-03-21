56a0d3d5465388d4d7bb148ee2487a4e22703e02
rinkishimo <rinkishimo@gmail.com>
2021-03-21 13:28:34 +0100

    Fixed minor code problems.

--- rcg-assignment-03-routing/src/App.js
+++ rcg-assignment-03-routing/src/App.js
@@ -16,13 +16,13 @@ class App extends Component {
           <ol style={{ textAlign: 'left' }}>
             <li>{done} Add Routes to load
             "<a href="/users">Users</a>" and "<a href="/courses">Courses</a>" on different pages (by entering a URL, without Links)</li>
-            <li>{done} Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
+            <li>{done} Add a simple navigation with two links =&gt; One leading to "Users", one leading to "Courses"</li>
             <li>{done} Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
             <li>{done} Pass the course ID to the "Course" page and output it there</li>
             <li>{done} Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
             <li>{done} Load the "Course" component as a nested component of "Courses"</li>
             <li>{done} Add a 404 error page and render it for any unknown routes</li>
-            <li>{done} Redirect requests to <a href="/all-courses">/all-courses</a> to /courses (=> Your "Courses" page)</li>
+            <li>{done} Redirect requests to <a href="/all-courses">/all-courses</a> to /courses (=&gt; Your "Courses" page)</li>
           </ol>
           <hr />
           <p>
